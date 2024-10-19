73ec0a404f01f97c889bcae0f24f8094f7b5dab7
okoeee <yk27.puxcm@gmail.com>
2024-10-19 23:11:57 +0900

    fix security group

--- web/terraform/modules/security/security_group.tf
+++ web/terraform/modules/security/security_group.tf
@@ -4,20 +4,29 @@ resource "aws_security_group" "default" {
   vpc_id = "${var.vpc_id}"
 }
 
+resource "aws_security_group_rule" "ingress_from_alb" {
+  type = "ingress"
+  from_port = "80"
+  to_port = "80"
+  protocol = "TCP"
+  security_group_id = aws_security_group.default.id
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "ingress_sg_all" {
+  type = "ingress"
   from_port = "0"
   to_port = "0"
   protocol = "-1"
   security_group_id = aws_security_group.default.id
   source_security_group_id = aws_security_group.default.id
-  type = "ingress"
 }
 
 resource "aws_security_group_rule" "egress_all" {
+  type = "egress"
   from_port = "0"
   to_port = "0"
   protocol = "-1"
   security_group_id = aws_security_group.default.id
-  type = "egress"
   cidr_blocks = ["0.0.0.0/0"]
 }
